18fb6d59572aa0970305af0d3e2a9c31ac912ff6
Michael Olusanya <michaelolusanya@gmail.com>
2022-10-14 17:41:02 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	char g;
+
+	g = 'a';
+	while
+		(g <= 'z')
+		{
+			putchar(g);
+			g++;
+		}
+
+	putchar('\n');
+	return (0);
+}
