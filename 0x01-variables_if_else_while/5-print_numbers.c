e54dbf6b6f2f09e3c96d0ebebb83788ae586ec8c
Michael Olusanya <michaelolusanya@gmail.com>
2022-10-14 17:53:08 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf("%d", c);
+	}
+	putchar('\n');
+	return (0);
+}
