b4d3d251d282d703c5de0ab68613d6d2926b8369
Michael Olusanya <michaelolusanya@gmail.com>
2022-11-02 06:02:11 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,16 +8,19 @@
 */
 int factorial(int n)
 {
+    int res ;
 	if (n < 0)
     {		
 		return (-1);
     }
-	else if (n == 0);
+	else if (n == 0)
     {
 		return (1);
     }
 	else
     {
-		return (n * factorial(n - 1));
+		res = n * factorial(n - 1) ;
     }
+
+    return (res)
 }
