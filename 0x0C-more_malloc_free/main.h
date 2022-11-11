954c22ed0ba7a9daa3c578aa1d2fefe6c4db41ed
Michael Olusanya <michaelolusanya@gmail.com>
2022-11-11 10:24:29 -0800

    correction

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,7 +8,7 @@ void init(char *str, int l);
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int l);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
